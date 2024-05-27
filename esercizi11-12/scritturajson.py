d9ac48802b3178ac930518241337ffd690bacb1a
Sassa1993 <118011618+MiraiTsukiGames@users.noreply.github.com>
2024-05-27 18:12:41 +0200

    file json

---
+++ esercizi11-12/scritturajson.py
@@ -0,0 +1,13 @@
+import json
+
+# Dati da scrivere nel file JSON
+dati = {
+    'nome': 'Mario Rossi',
+    'età': 30,
+    'indirizzo': 'Via Roma 123'
+}
+
+# Apertura del file JSON in modalità scrittura
+with open('dati.json', 'w') as file:
+    # Scrittura dei dati JSON
+    json.dump(dati, file)
